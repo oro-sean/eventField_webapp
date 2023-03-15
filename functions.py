b4999e33e8bd4a4b143ebc27446f936c1c11274b
oro-sean <oro.sean@gmail.com>
2023-03-16 06:21:36 +1100

    add comments

--- functions.py
+++ functions.py
@@ -1,4 +1,33 @@
 def find_starts(rawLog):
+	## Move to more generic log manipulations file
+	## make threshold and time after start to query default keyword args
+	
+	"""
+	find_starts takes a raw log file and returns a dictionary of race starts.
+	The dictionary keys are the start numbers and each key has a list with ther following atributes
+	- Time stamp (at gun)
+	- Pt Lat (at gun)
+	- Pt Lon (at gun)
+	- Stb Lat (at gun)
+	- Stb Lon (at gun)
+	- Mk Lat (5 seconds after)
+	- Mk Lon (5 seconds after)
+	
+	The start is also labeled a recall or race depending on hoe soon another start is found.
+	
+	The function looks for columns named as follows
+	
+	Tm2Gn - race timer (in seconds)
+	TimeStamp - time of day (pandas datetime object)
+	PtLat
+	PtLon
+	StbLat
+	StbLon
+	MkLat
+	MkLon
+	
+	"""
+	
     import statistics as stats
     import numpy as np
 
@@ -63,6 +92,21 @@ def find_starts(rawLog):
     # value [df index, Start time, port Lat, port long, stb lat, stb long, mark 1 lat, mark 1 long, start type]
 
 def PrepLog(logPath):
+	## move to more generic logmanipulation file
+	
+	"""
+	The PrepLog function takes a file path and loads a csv log file.
+	The old expedition log format was the template for this function but any csv with a header row and a row per observation will work.
+	
+	The column headers are then relabelled considering common synomons (ie "UTC" becomes "TimeStamp")
+	
+	The timestamp column is changed to a pandas datetime object assuming an original format of expedtion time.
+	the race counter is changed to float number in seconds (assuming it was in decimals of days - expedition standard)
+	
+	a pandas data fram with all raw log data as float64 type is returned (except timestamp - pandas date time)
+	
+	
+	"""
     import pandas as pd
 
     ## Import log and convert to generic veering format
@@ -118,6 +162,20 @@ def PrepLog(logPath):
     return rawLog
 
 def FindFeature(raceFrame, startIndex, startNo):
+	## good potential for a class
+	## tidy up logging so as it can be used
+	"""
+	Find race features takes a pandas data frame consisitng of all observation from one start until the next start.
+	The index and start no are also handed over.
+	the raceFrame also has a column 'Dist' which is the eucledian distance of the lat and long between the stating reference point
+	and the boats current position
+	
+	Find features looks for peaks and troughs in this distance and the gradient of the distance cure before and after.
+	It then tries to build a logical race orderted Top, botttom top ect ect to finish
+	
+	a dictionary of race features is returned as well as a log
+	
+	"""
     import numpy as np
     import scipy.signal as signal
 
@@ -168,6 +226,11 @@ def FindFeature(raceFrame, startIndex, startNo):
     return outputFeatures, outputLog
 
 def find_events(startDict, rawLog):
+	
+	"""
+	finds_events takes the raw log and a dictionary with all start properties.
+	
+	"""
     import numpy as np
 
     raceEvents = {} # create empty dictionary to store race events in
@@ -196,6 +259,8 @@ def find_events(startDict, rawLog):
             raceLengths.append(startDict[startNo+1][0]-startDict[startNo][0])
 
         raceFrame = rawLog.iloc[startDict[startNo][0]:endIndex] # create data frame of all lines between the last start and the end index
+				
+				###try except statement here if marks dont exist
         refLat = (startDict[startNo][2]+ startDict[startNo][4])/2 # find the average Lat between the pin and boat to use as reference point for distance calc
         refLon = (startDict[startNo][3]+ startDict[startNo][5])/2 # find the average Lon between the pin and boat to use as reference point for distance calc
         raceFrame = raceFrame.assign(Dist = list(np.square(raceFrame['Lat']-refLat) + np.square(raceFrame['Lon']-refLon)))  # calculate the distance from the ref point (midline) to the boats current location
@@ -270,7 +335,9 @@ def find_events(startDict, rawLog):
     ## Plot the track for each race for checking
 
 def plot_race(startDict, raceEvents, rawLog, marksDict, raceIndex):
-
+"""
+Plotting
+""" 
     import matplotlib.pyplot as plt
     import seaborn as sns
     import numpy as np
@@ -314,16 +381,28 @@ def plot_race(startDict, raceEvents, rawLog, marksDict, raceIndex):
     return plots
 
 def sails_from_rrp(path):
+	## move to rrp utils
+	## ad support for staysails
+	## sailtypes as defaults arguments
+	## make sails and manual vars a class??
+	"""
+	sails_from_rrp imports a rrp config (or event file??) from a location defined by "path".
+	The XML file is parsed and returns a dictionary containing all the individual sail codes for each sail type.
+	"""
     import xml.etree.ElementTree as ET
+		## import tree and get root
     tree = ET.parse(path)
     root = tree.getroot()
-
+	
+		## Empty lists for each sail type, pluys final dictionary
     mains = []
     jibs = []
     spins = []
     stays = []
     sail_dict = {}
-
+		
+		## find each sail with the "sailgroup" label corepsonding to the sail type and appened to
+		## the apropriate list and then add to dictionary
     for sail in root.findall("./sailinventory/item[@sailgroup='M']"):
         mains.append(sail.get('name'))
     sail_dict['mains'] = mains
@@ -336,6 +415,7 @@ def sails_from_rrp(path):
         spins.append(sail.get('name'))
     sail_dict['spins'] = spins
 
+		## make jibs available as staysails
     stays = jibs
     stays = ['']+stays
     sail_dict['stays'] = stays
@@ -344,6 +424,10 @@ def sails_from_rrp(path):
     return sail_dict
 
 def manualVars_from_rrp(path):
+	"""
+	manualVars_from_rrp inspects a rrp config file for all the manual entry variables defined.
+	It returns the variable names, types and categories (for categorical variables)
+	"""
     import xml.etree.ElementTree as ET
     tree = ET.parse(path)
     root = tree.getroot()
@@ -367,6 +451,21 @@ def manualVars_from_rrp(path):
     return categorical, integer, floatType
 
 def build_var_dict(sail_dict, categorical, integer, floatType):
+	## move to a eventField utils file
+	
+	"""
+	build_var_dict takes all inputed variable dictionaries and combines into a single dictionary formatted as follows (to suit build selector race)
+	
+	{Sails: {mains: [list of mainsails],
+					{jibs: [List of jibs],
+					......},
+	categorical: {categorical_1: [List of values],
+								categorical_2:[List of values],
+								....},
+	integer: [List of integer variables],
+	
+	float: [list of float variables]}
+	"""
     variables_dict = {'sails':sail_dict,
                       'categorical': categorical,
                       'integer': integer,
@@ -375,6 +474,12 @@ def build_var_dict(sail_dict, categorical, integer, floatType):
     return variables_dict
 
 def get_rrp_config(path, use_config):
+    ## move to eventfiled utils
+    """
+    get_rrp_config is a helper function that in future will allow users to input their own sail lists and variables.
+    
+    It returns a dictionary of all the manual entry variables and sails
+    """
 
     if use_config:
         sail_dict = sails_from_rrp(path)
@@ -395,40 +500,77 @@ def get_rrp_config(path, use_config):
     return var_dict
 
 def build_selectors_race(variables_dict):
-    import ipywidgets as widgets
+    ## possibly better as a class
+    
+    """
+    build_selectors_race take a dictionary of variables (in sails) strucured as follows
+    
+    {Sails: {mains: [list of mainsails],
+    {jibs: [List of jibs],
+    ......},
+    categorical: {categorical_1: [List of values],
+    categorical_2:[List of values],
+    ...},
+    integer: [List of integer variables],
 
+    float: [list of float variables]}
+
+    The apropriate widget type is then created for each variable / sail selector
+    
+    These are then arranged into horizontal and vertical boxes
+
+    The sail widgets are grouped horizontally across.
+    The manual variable widgets are grouped vertically as categorical, integer and float.
+    The 3 vertical boxes are then group horizontally.
+    Finally the sail widgets are stacked ontop of the manual variable widgets.
+
+    Then all the individual widgets and the parent formatted widget are returned as a dictionary
+
+    """
+    import ipywidgets as widgets
+    
+    ## create individual dictionaries for sails and each variable 
     sail_dict = variables_dict['sails']
     categorical = variables_dict['categorical']
     integer = variables_dict['integer']
     floatType = variables_dict['floatType']
 
+    ## create Select type widgets for the sails
     sail_selectors = {}
     for sailType in sail_dict.keys():
         sail_selectors[sailType] = widgets.Select(options=sail_dict[sailType],
                                               description=sailType)
+    ## create Dropdown type widgets for all categorical variables
     categorical_selectors = {}
     for catVariable in categorical.keys():
         categorical_selectors[catVariable] = widgets.Dropdown(options=categorical[catVariable],
                                                           description=catVariable)
-
+    ## arrange all categorical Dropdown widgets vertically in a box
     left_box = widgets.VBox([item for item in categorical_selectors.values()])
-
+    
+    ## integer variable widgets are crerated using IntText
     int_selectors = {}
     for intVariable in integer:
         int_selectors[intVariable] = widgets.IntText(description=intVariable)
 
+    ## the middle manual variable are grouped vertically in a box
     mid_box = widgets.VBox([item for item in int_selectors.values()])
 
+    ## the float selectors widgets are created using the FloatText widget
     float_selectors = {}
     for floatVariable in floatType:
         float_selectors[floatVariable] = widgets.FloatText(description=floatVariable)
 
+    ## the right manual variables are grouped together
     right_box = widgets.VBox([widget for widget in float_selectors.values()])
 
+    ## All 3 manual entry variable boxes are grouped together horizontally
     bottom_box = widgets.HBox([left_box, mid_box, right_box])
 
+    ## All sail selector widgets are grouped together horizontally 
     top_box = widgets.HBox([widget for widget in sail_selectors.values()])
 
+    ## the 2 horizontal boxes are stacked ontop of eachother to create the layout for all race widgets.
     race_tab = widgets.VBox([top_box, bottom_box])
 
     selectors_dict = {'sails':sail_selectors,
@@ -440,21 +582,35 @@ def build_selectors_race(variables_dict):
     return selectors_dict
 
 def build_selectors_day(raceCount, variables_dict):
+    """
+    Racecount = integer # of races
+    variables Dict = dictionary of sails and manual variables.
+    
+    Build_selectors_day essentially just calls "build_selector_dict_race" for each race and combines this into a tab per race.
+    
+    Directional links are also generated so as changes made in a given race are pushed to all future races but not updated for previous races.
+
+    All widgets and the formatted widgets and directional links are then returned as a dictionary
+
+    
+    """
     import ipywidgets as widgets
 
+    ## iterate over each start, generate widgets and make tabs
     selectors_dict_day = {}
     for i in range(raceCount):
-        selectors_dict_day[i+1] = build_selectors_race(variables_dict)
+        selectors_dict_day[i+1] = build_selectors_race(variables_dict) # use build_race_selector to build the selectors for each race
 
-    race_numbers = [int for int in range(1,raceCount+1)]
-    race_tabs = [selectors_dict_day[i]['formatted'] for i in race_numbers]
-    tab_names = ['Race '+str(i) for i in race_numbers]
-    selector_tabs = widgets.Tab()
-    selector_tabs.children = race_tabs
-    [selector_tabs.set_title(i, title) for i, title in enumerate(tab_names)]
+    race_numbers = [int for int in range(1,raceCount+1)] # create list of race numbers
+    race_tabs = [selectors_dict_day[i]['formatted'] for i in race_numbers] # create a tab for each race
+    tab_names = ['Race '+str(i) for i in race_numbers] # create list of tb names
+    selector_tabs = widgets.Tab() # generate tabs
+    selector_tabs.children = race_tabs # assign content to tabs
+    [selector_tabs.set_title(i, title) for i, title in enumerate(tab_names)] # set tab titles
 
-    selectors_dict_day['formatted'] = selector_tabs
+    selectors_dict_day['formatted'] = selector_tabs # add the formatted tabs to the dictionary for use later
 
+## Generate directional links between tabs
     directional_links_dict = {}
     for race in list(selectors_dict_day.keys())[:-2]:
         directional_links_dict[race]={}
@@ -468,6 +624,10 @@ def build_selectors_day(raceCount, variables_dict):
     return selectors_dict_day
 
 def load_and_process(log_path, config_path,use_config):
+    ## move to eventfield utils file
+    """
+    loand_and_process is a simple worker function that executes the eventfield math and generates the selector widgets.
+    """
     var_dict=get_rrp_config(config_path,use_config)
     log = PrepLog(log_path)
     startDict = find_starts(log) # obtain dictionary with start details
@@ -477,6 +637,8 @@ def load_and_process(log_path, config_path,use_config):
     return log, var_dict, startDict, raceIndex, marksDict, raceEvents, eventLog, selector_dict_day
 
 def update_default_chooser_path(chooser):
+    ## move to eventfield utils
+    
     if chooser.selected:
         chooser.default_path = chooser.selected_path
         chooser.title = chooser.title
@@ -485,6 +647,11 @@ def update_default_chooser_path(chooser):
 
         
 def build_top_pane():
+    ## rename as not top pane any more
+    
+    """
+    Function builds the top pane by horizontally stacking the file choosers for the log file and the config file
+    """
     from ipyfilechooser import FileChooser
     import ipywidgets as widgets
 
@@ -510,7 +677,15 @@ def build_top_pane():
     return top_pane_dict
 
 def mid_pane_handler(top_pane_dict, loaded):
+## this needs to be updated to show error log when incorrect files are loaded
+
+    """
+    Handler function that either retunrs a label widget showing "please load files" until the log and config are loaded and processed. Once this happens the plots and race tabs are displayed 
+    """
+
     import ipywidgets as widgets
+
+    ## if loaded boolean is False displaye msg
     if not loaded:
         not_loaded = [widgets.Label('Please Load Files')]
         mid_pane = widgets.Box(not_loaded)
@@ -518,10 +693,12 @@ def mid_pane_handler(top_pane_dict, loaded):
         raceEvents = []
         startDict = []
         log = []
-        
+    
+    ## loaded boolean is True call load_and_process worker and create the middle pane as a vertical box with the plots tab and selectors tabs stacked ontop of eachother
     if loaded:
         selector_dict_day, plot_tab, log, raceEvents, marksDict, startDict = functions.load_and_process_worker(top_pane_dict)
-        mid_pane = widgets.VBox([plot_tab,selector_dict_day['formatted']])
+        
+    mid_pane = widgets.VBox([plot_tab,selector_dict_day['formatted']])
         
     display(mid_pane)
 
@@ -529,6 +706,10 @@ def mid_pane_handler(top_pane_dict, loaded):
     return selector_dict_day, raceEvents, startDict, log
 
 def build_plot_tabs(plots):
+    
+    """
+    function to build a tab for each race to be plot. Plots arte handed to this function from the plot generator function
+    """
     import ipywidgets as widgets
     from IPython.display import display
     
@@ -548,6 +729,11 @@ def build_plot_tabs(plots):
     return plot_tab
 
 def load_and_process_worker(top_pane_dict):
+    ## move to event app utils
+    """
+    Worker function to take the inputs from the top tab and call the load and process and plot functions.
+    The function returns the selectors, plot tabs aswell as the event and mark dicts.
+    """
     use_config = top_pane_dict['buttons'][1].value
     log_path = top_pane_dict['file_choosers'][0].selected
     config_path = top_pane_dict['file_choosers'][1].selected
@@ -585,6 +771,9 @@ def tidy_last_race_event(raceEvents):
     return raceEvents
 
 def race_events_for_xml(raceEvents, event_values, log):
+    """
+    takes the race events dict and event values and creates the mark rounding and sails up events for during the race. These events are returned as a list of lists
+    """
 
     events_for_XML = []
 
@@ -622,6 +811,9 @@ def race_events_for_xml(raceEvents, event_values, log):
     return events_for_XML
 
 def start_events_for_xml(startDict, log, event_values):
+    """
+    Builds a list of lists containg all the start events plus prestart sails up and manual entry variables.
+    """
     import pandas as pd
     events_for_XML = []
     for start in startDict.keys():
@@ -650,7 +842,10 @@ def start_events_for_xml(startDict, log, event_values):
     return events_for_XML
 
 def events_for_xml(raceEvents, startDict, event_values, log):
-
+    ## move to eventField utils
+    """
+    events_for_xml is a simple worker function which calls a serries of functions to generate all the events for the days sailing and returns them as a single list ready for exporting to HTML
+    """
     raceEvents = tidy_last_race_event(raceEvents)
 
     events_from_race = race_events_for_xml(raceEvents, event_values, log)
@@ -665,15 +860,13 @@ def events_for_xml(raceEvents, startDict, event_values, log):
     
     events_list = events_list + [[str(start.date()),str(start.time()),"DayStart",""]] + [[str(stop.date()),str(stop.time()),"DayStop",""]]
     
-
-
-    
-    
-
-
     return events_list
 
 def write_xml(events_list, filePath_export, fileName_export, boatName):
+    ## rrp utils
+    """
+    write_xml takes a list of events and writes an XML file to suit SP RRP. The XML file is exported directly to the specified path
+    """
     events_list.sort(key=lambda x: x[1])
     import xml.etree.ElementTree as ET
     export_root = ET.Element("daysail")
@@ -707,7 +900,7 @@ def write_xml(events_list, filePath_export, fileName_export, boatName):
     print('Event File exported as '+str(fileName_export))
 
 def export_to_xml_worker(selector_dict_day, raceEvents, startDict, log, filePath_export, fileName_export, boatName):
-
+    ## move to eventField utils
     event_values = get_event_values(selector_dict_day)
 
     events_list_for_xml = events_for_xml(raceEvents, startDict, event_values, log)
